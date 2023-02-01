492a934e63564b4ffa60bd0f56268ee8352c043d
lberrymage <logan@lberrymage.dev>
2023-01-31 17:49:14 -0700

    Remove sample tests

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -20,53 +20,10 @@ repositories {
     mavenCentral()
 }
 
-testing {
-    suites {
-        // Configure the built-in test suite
-        val test by getting(JvmTestSuite::class) {
-            // Use Kotlin Test test framework
-            useKotlinTest("1.7.10")
-
-            dependencies {
-                // Use newer version of JUnit Engine for Kotlin Test
-                implementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-            }
-        }
-
-        // Create a new test suite
-        val functionalTest by registering(JvmTestSuite::class) {
-            // Use Kotlin Test test framework
-            useKotlinTest("1.7.10")
-
-            dependencies {
-                // functionalTest test suite depends on the production code in tests
-                implementation(project())
-
-                // Use newer version of JUnit Engine for Kotlin Test
-                implementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-            }
-
-            targets {
-                all {
-                    // This test suite should run after the built-in test suite has run its tests
-                    testTask.configure { shouldRunAfter(test) } 
-                }
-            }
-        }
-    }
-}
-
 gradlePlugin {
     // Define the plugin
     val greeting by plugins.creating {
         id = "app.accrescent.tools.bundletool.greeting"
         implementationClass = "app.accrescent.tools.bundletool.BundletoolGradlePluginPlugin"
     }
-}
-
-gradlePlugin.testSourceSets(sourceSets["functionalTest"])
-
-tasks.named<Task>("check") {
-    // Include functionalTest as part of the check lifecycle
-    dependsOn(testing.suites.named("functionalTest"))
 }
