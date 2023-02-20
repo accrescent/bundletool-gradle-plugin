cdf7bd5233127fdbcfff5d4b2a88ebedacaa9f32
lberrymage <logan@lberrymage.dev>
2023-02-20 13:19:34 -0700

    Bump version to 0.1.2

--- bundletool-gradle-plugin/build.gradle.kts
+++ bundletool-gradle-plugin/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 group = "app.accrescent.tools"
-version = "0.1.1"
+version = "0.1.2"
 
 gradlePlugin {
     website.set("https://github.com/accrescent/bundletool-gradle-plugin")
