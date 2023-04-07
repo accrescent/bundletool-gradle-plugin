06bef67578c6b919aea96af75e0afe040e6031cc
lberrymage <logan@lberrymage.dev>
2023-04-07 13:43:26 -0700

    Bump Gradle publish plugin from 1.1.0 to 1.2.0

--- bundletool-gradle-plugin/build.gradle.kts
+++ bundletool-gradle-plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("com.gradle.plugin-publish") version "1.1.0"
+    id("com.gradle.plugin-publish") version "1.2.0"
     id("org.jetbrains.kotlin.jvm") version "1.8.20"
 }
 
