98040fb4152299cb7af9d073aef3ecb7c214309f
lberrymage <logan@lberrymage.dev>
2023-09-04 11:00:33 -0700

    Remove unused import

--- bundletool-gradle-plugin/src/main/kotlin/app/accrescent/tools/bundletool/SigningConfig.kt
+++ bundletool-gradle-plugin/src/main/kotlin/app/accrescent/tools/bundletool/SigningConfig.kt
@@ -2,7 +2,6 @@ package app.accrescent.tools.bundletool
 
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
-import java.io.File
 
 abstract class SigningConfig {
     abstract val storeFile: RegularFileProperty
