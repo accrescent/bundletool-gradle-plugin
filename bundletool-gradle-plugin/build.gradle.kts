b274e4578c66fdd2d97bff88b3b4dc7f2dcc383c
lberrymage <logan@lberrymage.dev>
2024-01-05 22:24:52 -0700

    Bump com.android.tools:common from 31.2.0 to 31.2.1

--- bundletool-gradle-plugin/build.gradle.kts
+++ bundletool-gradle-plugin/build.gradle.kts
@@ -34,7 +34,7 @@ kotlin {
 }
 
 dependencies {
-    compileOnly("com.android.tools:common:31.2.0")
+    compileOnly("com.android.tools:common:31.2.1")
     compileOnly("com.android.tools:sdklib:31.2.0")
     compileOnly("com.android.tools.build:bundletool:1.15.6")
     compileOnly("com.android.tools.build:gradle-api:8.2.1")
